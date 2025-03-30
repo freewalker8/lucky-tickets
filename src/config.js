be7dd3e1cae48f916f91af639fedec9c03d8cbd1
lianglei <stone.ll@qq.com>
2025-03-30 21:45:16 +0800

    feat:支持配置每期追投投注

--- src/config.js
+++ src/config.js
@@ -3,7 +3,15 @@
  */
 
 module.exports = {
-    executablePath: "C:\\Users\\stone\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe", // 本地浏览器路径
-    headless: true, // 可视化模式（调试时建议开启，设置为false）
-    // devtools: true, // 是否开启调试模式
+    // 本地浏览器路径
+    executablePath: "C:\\Users\\stone\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe",
+    // 可视化模式（调试时建议开启，设置为false）
+    headless: true,
+    // 是否开启调试模式
+    // devtools: true,
+    // 追踪的彩票号，有值时生成的投注号码需包含追投号码
+    trackingTickets: [
+        [5, 6, 8, 12, 25, 29, 7],
+        [6, 8, 12, 19, 25, 29, 6],
+    ],
 }
--- src/genLuckyNumbers.js
+++ src/genLuckyNumbers.js
@@ -1,3 +1,5 @@
+const { trackingTickets } = require('./config');
+
 /**
  * 将开奖记录展开，分别放置到红球组和蓝球组
  * @param {Array<Array<Number>>} pageNumbers 开奖记录
@@ -58,15 +60,28 @@ function _genOneTicket(flattenedRedNumbers, flattenedBlueNumbers) {
 /**
  * 根据历史开奖记录生成随机的幸运投注号码
  * @param {Array<Array<Number>>} pageNumbers 历史开奖记录
- * @param {Number} mount 投注数
+ * @param {Number} count 投注数
  * @returns {Array<Array<Number>>} 幸运投注号码
  */
-function genLuckyNumbers(pageNumbers, mount) {
+function genLuckyNumbers(pageNumbers, count) {
   const luckyNumbers = [];
   const [flattenedRedNumbers, flattenedBlueNumbers] = _flattenNumbers(pageNumbers);
+  // 随机生成的投注数
+  let autoGenCount = count;
+
+  // 设置了每期追投的投注号码
+  if (Array.isArray(trackingTickets) && trackingTickets.length) {
+    luckyNumbers.push(...trackingTickets);
+    autoGenCount = count - trackingTickets.length;
+
+    if (trackingTickets.length >= count) {
+      console.warn('warning:每期追投的投注号码数量大于总投注数，不会生成随机投注号码');
+      return luckyNumbers;
+    }
+  }
 
   // console.log('flattenedRedNumbers, flattenedBlueNumbers', flattenedRedNumbers, flattenedBlueNumbers);
-  for (let i = 0; i < mount; i++) {
+  for (let i = 0; i < autoGenCount; i++) {
     luckyNumbers.push(_genOneTicket(flattenedRedNumbers, flattenedBlueNumbers));
   }
   return luckyNumbers;
